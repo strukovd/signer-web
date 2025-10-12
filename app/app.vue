32a83567bb251586ac3f4db657807031005f8f48
strukovd <dmitryi97@yandex.ru>
2025-10-13 03:11:03 +0600

    GAZ-1560: refactored new project

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<NuxtLayout>
+		<NuxtPage/>
+	</NuxtLayout>
 </template>
+
+<style lang="scss">
+body {
+	div {
+
+	}
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<section class="main-page">
+test
+	</section>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss">
+
+</style>
