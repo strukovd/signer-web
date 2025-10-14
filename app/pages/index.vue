6825f19dc94709eb1348c7615b3c2d25adef2bb0
strukovd <dmitryi97@yandex.ru>
2025-10-14 17:50:37 +0600

    GAZ-1572: added pdf page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,39 @@
 <template>
 	<section class="main-page">
-test
+		<main class="PDF-wrapper">
+			<PDF path="file.pdf"/>
+		</main>
+		<footer class="options" style="padding:1em; display:flex;">
+			<BaseButton style="flex:auto 1 0;" append-icon="mdi-arrow-right">ДАЛЕЕ</BaseButton>
+		</footer>
 	</section>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import BaseButton from '~/components/common/BaseButton.vue';
+
 
 </script>
 
 <style lang="scss">
+.main-page {
+	height: 100vh;
+	// margin:auto auto;
 
+	.PDF-wrapper {
+		height:90vh;
+		overflow:auto;
+		padding:1em;
+	}
+
+	.options {
+		.base-button {
+			.button-text {
+				font-family: "Carlito", Segoe UI, Tahoma, Geneva, Verdana, sans-serif;
+				line-height:2em;
+				font-weight:700;
+			}
+		}
+	}
+}
 </style>
