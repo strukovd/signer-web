bbdb7a5432104b442729aefbb3072e3d046e5fd9
strukovd <dmitryi97@yandex.ru>
2025-10-14 17:50:22 +0600

    GAZ-1572: added vue-pdf-embed component wrapper

---
+++ app/components/PDF.vue
@@ -0,0 +1,24 @@
+<template>
+	<ClientOnly>
+		<VuePdfEmbed
+			:source="path"
+			annotation-layer
+			text-layer
+		/>
+	</ClientOnly>
+</template>
+
+<script lang="ts" setup>
+	import VuePdfEmbed from 'vue-pdf-embed';
+	import 'vue-pdf-embed/dist/styles/annotationLayer.css';
+	import 'vue-pdf-embed/dist/styles/textLayer.css';
+
+	const props = defineProps({
+		path: String
+	});
+
+</script>
+
+<style>
+
+</style>
