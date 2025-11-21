20d9d9e735a7f54857734497fd38f9115d663954
strukovd <dmitryi97@yandex.ru>
2025-11-21 17:38:34 +0600

    GAZ-1644: refactored rating page

--- app/pages/rating.vue
+++ app/pages/rating.vue
@@ -1,73 +1,112 @@
 <template>
 	<section class="rating-page">
-		<main class="rating-wrapper">
-			<h1>Оцените качество работы оператора</h1>
-			<div class="grades">
-				<div style="background:#d30327;" class="grade">1</div>
-				<div style="background:#ea0b0c;" class="grade">2</div>
-				<div style="background:#f13609;" class="grade">3</div>
-				<div style="background:#f97604;" class="grade">4</div>
-				<div style="background:#f6b304;" class="grade">5</div>
-				<div style="background:#f7dd04;" class="grade">6</div>
-				<div style="background:#cedd12;" class="grade">7</div>
-				<div style="background:#8ad523;" class="grade">8</div>
-				<div style="background:#44bc37;" class="grade">9</div>
-				<div style="background:#14ab4a;" class="grade">10</div>
-			</div>
+		<main>
+			<section>
+				<div class="rating-title">Оцените качество работы оператора</div>
+				<div v-if="appStore.currentUser?.fullName" class="rating-username">
+					<span>{{ appStore.currentUser.fullName }}</span>
+				</div>
+			</section>
+			<section>
+				<div class="grades">
+					<div v-for="(color, index) of [ `#d30327`, `#ea0b0c`, `#f13609`, `#f97604`, `#f6b304`, `#f7dd04`, `#cedd12`, `#8ad523`, `#44bc37`, `#14ab4a` ]"
+						:key="index"
+						:style="{ background: color }"
+						class="grade"
+						@click="rate(index + 1)">
+							{{ index + 1 }}
+						</div>
+				</div>
+			</section>
 		</main>
-		<footer class="footer">
-			<BaseButton @click="appStore.nextPage()" style="flex:auto 1 0;" append-icon="mdi-arrow-right">ДАЛЕЕ</BaseButton>
-		</footer>
 	</section>
 </template>
 
 <script lang="ts" setup>
-import BaseButton from '~/components/common/BaseButton.vue';
+import BaseIcon from '~/components/common/BaseIcon.vue';
+
+const { $api } = useNuxtApp();
 
 const appStore = useAppStore();
+
+async function rate(grade: number) {
+	const issueKey = appStore.pageParams().issueKey;
+	const operator = appStore.login;
+
+	$api('v1/office-app/rating', {
+		headers: { 'auth-token': localStorage.getItem('token') } as Record<string, string>,
+		method: 'POST',
+		body: {
+			issueKey,
+			grade: 10,
+			operator: operator,
+		},
+	})
+		.then((data) => {
+			appStore.nextPage();
+			// if(data.accessToken) {
+			// 	useUserStore().setData(data);
+			// 	navigateTo('/');
+			// }
+		})
+		// .catch((err: FetchError) => {
+		// 	error.value = err.message;
+		// });
+}
 </script>
 
 <style lang="scss">
 .rating-page {
-	.rating-wrapper {
-		h1 {
-			text-align: center;
-			font-size: 2.4em;
-			font-weight: 700;
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 2em;
+
+		section:nth-child(1) {
+			text-align:center;
+
+			.rating-title {
+				margin-bottom:.3em;
+				text-align: center;
+				font-size: 2.4em;
+				font-weight: 700;
+			}
+			.rating-username {
+				opacity:.5;
+				line-height:1.4em;
+				text-align: center;
+				font-size: 1.4em;
+				font-weight: 700;
+			}
 		}
-		.grades {
-			display: flex;
-			flex-direction: row;
-			justify-content: center;
-			align-items: end;
-			gap: 1em;
+		section:nth-child(2) {
+			.grades {
+				display: flex;
+				flex-direction: row;
+				justify-content: center;
+				align-items: end;
+				gap: 1em;
 
 
-			.grade {
-				text-align: center;
-				line-height: 2em;
-				width:2em;
-				color: #fff;
-				font-size:3.4dvw;
-				text-align: center;
-				border-radius:30%;
-				cursor: pointer;
+				.grade {
+					text-align: center;
+					line-height: 2em;
+					width:2em;
+					color: #fff;
+					font-size:3.4dvw;
+					text-align: center;
+					border-radius:30%;
+					cursor: pointer;
 
-				&:hover {
-					opacity: .7;
+					&:hover {
+						opacity: .7;
+					}
 				}
 			}
 		}
 	}
 
-	.footer {
-		.base-button {
-			.button-text {
-				font-family: "Carlito", Segoe UI, Tahoma, Geneva, Verdana, sans-serif;
-				line-height:2em;
-				font-weight:700;
-			}
-		}
-	}
 }
 </style>
