c6f918694aecf9262204c38059cf6f361e4dc7f4
strukovd <strukovd@users.noreply.github.com>
2025-11-24 15:03:50 +0000

    deploy: 91d6a83a1a3f621ed443b67e94a0a11cf10645ee

--- _nuxt/ulpjPPML.js
+++
@@ -1,1 +0,0 @@
-import{d as t,x as o,y as s,V as a}from"#entry";const n={class:"default-layout"},_=t({__name:"default",setup(l){return(e,r)=>(s(),o("section",n,[a(e.$slots,"default")]))}});export{_ as default};
