6bbcd3f953514b76667882aa81bd3dda4d1a210c
strukovd <dmitryi97@yandex.ru>
2026-01-23 11:52:12 +0600

    updated deploy script

--- towild.sh
+++ towild.sh
@@ -2,17 +2,22 @@
 
 set -euo pipefail
 
-ARCHIVE_NAME="signer-build.tar.gz"
+APP_NAME="signer"
+ARCHIVE_NAME="${APP_NAME}-build.tar.gz"
 REMOTE_TMP="/tmp/${ARCHIVE_NAME}"
 REMOTE_HOST="root@5.23.48.222"
 TARGET_DIR="/var/www/signer"
 
+
+echo "📦 Building application"
 npm run build
 
 tar -czf "${ARCHIVE_NAME}" -C .output/public .
 
+echo "🏗️  Moving archive to ${REMOTE_HOST}:${TARGET_DIR}"
 scp "${ARCHIVE_NAME}" "${REMOTE_HOST}:${REMOTE_TMP}"
 
+echo "🚀 Deploying archive..."
 ssh -t "${REMOTE_HOST}" "\
 	rm -rf ${TARGET_DIR} && \
 	mkdir -p ${TARGET_DIR} && \
@@ -20,3 +25,6 @@ ssh -t "${REMOTE_HOST}" "\
 	rm -f ${REMOTE_TMP}"
 
 rm -f "${ARCHIVE_NAME}"
+
+
+echo "✅ Deployed to ${REMOTE_HOST}:${TARGET_DIR}"
